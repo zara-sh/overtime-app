88207bcd6a6688b7f433381de4ab8b36a8f4769d
zara-sh <zara.regist@gmail.com>
2018-01-15 17:43:36 +0900

    Userd resoure generator for post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "Creation" do
+    before do
+      @post = Post.create(date: Date.today, rationale:"Anything")
+    end
+      it "can be created" do
+      expect(@post).to be_valid
+    end
+    it "cantnot be created withoutd date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,15 +1,3 @@
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
-
-
-
-ActiveRecord::Migration.maintain_test_schema!
-
-# spec/rails_helper.rb
-
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
